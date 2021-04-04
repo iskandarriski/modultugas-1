35e7922f29202d1a9572b4415a1bc927d2943a30
iskandarriski <81105032+iskandarriski@users.noreply.github.com>
2021-04-04 23:38:56 +0700

    Add files via upload

---
+++ Looping.java
@@ -0,0 +1,12 @@
+package modultugas1;
+
+public class Looping {
+    public static void main(String[] args){
+        String[] siswa = {"Reinan", "Odena", "Geanno"}; //panjang array 3
+        
+            for (int i=0; i < siswa.length; i++){
+            System.out.println("Indeks ke " + i + " = " + siswa[i]);
+            }
+    }
+    
+}
---
+++ NestedLoop.java
@@ -0,0 +1,15 @@
+package modultugas1;
+
+public class NestedLoop {
+    public static void main (String[] args){
+        
+        int x,y;
+        for (x=0; x<=4; x++){
+            for (y=0; y<x; y++){
+                System.out.print(x);
+            }
+        System.out.println ();
+            }
+        }
+    
+}
